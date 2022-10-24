32bdc2fc75da6dce49ee8c1fc282b4da7f46506e
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-10-24 16:58:38 +0200

    deleted

--- _felev_anyaga/html1/fejresz/fejresz.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  var szinek = ["white", "black"];
-  var idx = 0;
-  setInterval(function() {
-    idx = 1-idx;
-    document.body.style="background-color: "+szinek[idx];
-  }, 1000);
-}
