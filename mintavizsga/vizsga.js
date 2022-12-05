924ca56dd9c98fd5f34c21f658b57d7bcaeefc3d
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-12-05 17:32:52 +0100

    JS/2

--- mintavizsga/vizsga.js
+++ mintavizsga/vizsga.js
@@ -13,12 +13,32 @@ function frissit(sor, oszlop) {
     document.getElementById("o" + oszlop).textContent = oszloposszeg
 }
 
+function szinez() {
+    let oliverDb = +document.getElementById("s0").textContent
+    let paliDb = +document.getElementById("s1").textContent
+    let oliver = document.querySelectorAll("tr")[2].children[1]
+    let pali = document.querySelectorAll("tr")[3].children[0]
+
+    if ( oliverDb >= paliDb*2 ) {
+        oliver.style = "color: red"
+    } else {
+        oliver.style = "color: black"
+    }
+
+    if ( paliDb >= oliverDb*2 ) {
+        pali.style = "color: red"
+    } else {
+        pali.style = "color: black"
+    }
+}
+
 window.addEventListener("load", () => {
     let mezok = document.querySelectorAll("input[type='number']")
     for ( let mezo of mezok ) {
         mezo.addEventListener("change", (e) => {
             let tomb = e.target.id.split(':')
-            frissit(+tomb[1], +tomb[2]) 
+            frissit(+tomb[1], +tomb[2])
+            szinez()
         })
     }
 })
